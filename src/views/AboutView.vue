095eff91305b64e18a907de38af9b93854c602b5
hex4coder <the.programmer.luyo@gmail.com>
2024-11-22 06:27:49 +0800

    init projects

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>Created By :</h1>
+    <h3>Ardan, S.Kom</h3>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/LinksGeneratorView.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Links generator</h1>
+</template>
+<script setup></script>
